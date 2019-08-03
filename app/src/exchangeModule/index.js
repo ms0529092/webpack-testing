584671cddf573a676a2a842a1820c5a6cbc651f9
JungHanslee <echa0771@gmail.com>
2019-08-03 21:50:17 +0800

    no message

---
+++ app/src/exchangeModule/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div>
+                <h1> Hello, Exchange! </h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('exchange'));
